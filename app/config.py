7dc397195c408f2e65dcc6a5b19e86d61640ff54
Suat Bayır <suatbayir1@gmail.com>
2025-04-25 20:53:40 +0300

    [11] updated ci&cd pipeline

--- app/config.py
+++ app/config.py
@@ -6,9 +6,6 @@ class Settings(BaseSettings):
     """The environment variables used by the application."""
 
     EMBEDDING_MODEL_NAME: str = Field(description="Embedding model name")
-    VECTOR_DB_PATH: str = Field(description="Path to the vector database")
-    REPO_CLONE_DIRECTORY: str = Field(description="Path to the repository clone directory")
-    VECTOR_DB_DEFAULT_DIMENSION: int = Field(description="Default dimension of the vector database")
     USE_GPU: bool = Field(description="Use GPU for processing")
 
     CHROMA_HOST: str = Field(description="ChromaDB host")
